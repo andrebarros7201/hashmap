0403d7111b3904be5f06048692181d6ee88a9d76
Andre Barros <andrebarros7201@gmail.com>
2024-05-25 18:53:56 +0100

    insert set resize methods

--- src/hashmap.js
+++ src/hashmap.js
@@ -1,17 +1,16 @@
 class Node {
-  constructor(key, value = null) {
+  constructor(key, value, next = null) {
     this.key = key;
     this.value = value;
-    this.next = null;
+    this.next = next;
   }
 }
 
 class HashMap {
   constructor() {
     this.loadFactor = 0.75;
-    this.bucketArray = new Array(16).fill(null);
-    this.capacity = this.bucketArray.length;
-    this.occupied = 0;
+    this.bucketArray = new Array(16);
+    this.size = 0;
   }
 
   hash(key) {
@@ -22,4 +21,49 @@ class HashMap {
     }
     return hashCode;
   }
+
+  _resize() {
+    const oldArray = this.bucketArray;
+    const newArray = new Array(this.bucketArray.length * 2);
+    this.bucketArray = newArray;
+    this.size = 0;
+
+    for (let bucket of oldArray) {
+      let node = bucket;
+      while (node) {
+        this._insert(node.key, node.value, newArray);
+        node = node.next;
+      }
+    }
+  }
+
+  _insert(key, value, array) {
+    const index = this.hash(key);
+    if (!array[index]) {
+      array[key] = new Node(key, value);
+    } else {
+      let node = array[index];
+      while (node) {
+        if (node.key === key) {
+          node.value = value;
+          return;
+        }
+        if (!node.next) {
+          node.next = new Node(key, value);
+          return;
+        }
+        node = node.next;
+      }
+    }
+    this.size++;
+  }
+
+  set(key, value) {
+    if (this.size / this.bucketArray.length >= this.loadFactor) {
+      this._resize();
+    }
+    this._insert(key, value, this.bucketArray);
+  }
 }
+
+export { HashMap };
--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log("Test");
+import { HashMap } from "./hashmap";
+
+let hashmap = new HashMap();
+console.log(hashmap);
+
+hashmap.set(1, 1);
+hashmap.set(2, 1);
+hashmap.set(3, 1);
+hashmap.set(4, 1);
+hashmap.set(5, 1);
+hashmap.set(6, 1);
+hashmap.set(7, 1);
+hashmap.set(8, 1);
+hashmap.set(9, 1);
+console.log(hashmap);
+
+hashmap.set(10, 1);
+hashmap.set(11, 1);
+hashmap.set(12, 1);
+hashmap.set(13, 1);
+hashmap.set(14, 1);
+hashmap.set(15, 1);
+hashmap.set(16, 1);
+console.log(hashmap);
