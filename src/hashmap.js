4c66b52ebba2387f82083b04380c31f63a64d643
Andre Barros <andrebarros7201@gmail.com>
2024-05-25 23:28:04 +0100

    Final

--- src/hashmap.js
+++ src/hashmap.js
@@ -8,88 +8,90 @@ class Node {
 
 class HashMap {
   constructor() {
-    this.size = 0;
-    this.array = new Array(16);
+    this.keyMap = new Array(16);
+    this.size = 0; // Number of key-value pairs in the map
     this.loadFactor = 0.75;
   }
 
-  _hash(key) {
-    let hashCode = 0;
-    const primeNumber = 31;
-    for (let i = 0; i < key.length; i++) {
-      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+  _hash(key, keyMapLength) {
+    let total = 0;
+    let PRIME = 31; // Prime number helps in spreading the keys more uniformly
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96; // Assuming keys are lowercase strings
+      total = (total * PRIME + value) % keyMapLength;
     }
-    return hashCode;
+    return total;
   }
 
   _resize() {
-    const oldArray = this.array;
-    let newArray = new Array(this.array.length * 2);
-    this.array = newArray;
+    const newKeyMap = new Array(this.keyMap.length * 2);
+    const oldKeyMap = this.keyMap;
+    this.keyMap = newKeyMap;
     this.size = 0;
 
-    for (const bucket of oldArray) {
+    for (let bucket of oldKeyMap) {
       let node = bucket;
       while (node) {
-        this._insert(node.key, node.value);
+        this._insert(node.key, node.value, newKeyMap);
         node = node.next;
       }
     }
   }
 
-  _insert(key, value) {
-    const index = this._hash(key);
-
-    if (!this.array[index]) {
-      this.array[index] = new Node(key, value);
+  _insert(key, value, keyMap) {
+    const index = this._hash(key, keyMap.length);
+    if (!keyMap[index]) {
+      keyMap[index] = new Node(key, value);
     } else {
-      let node = this.array[index];
-      if (node.key === key) {
-        node.value = value;
-        return;
+      let node = keyMap[index];
+      while (node) {
+        if (node.key === key) {
+          node.value = value; // Update value if key already exists
+          return;
+        }
+        if (!node.next) {
+          node.next = new Node(key, value);
+          return;
+        }
+        node = node.next;
       }
-      if (!node.next) {
-        node.next = new Node(key, value);
-        return;
-      }
-      node = node.next;
     }
     this.size++;
   }
 
   set(key, value) {
-    if (this.size / this.array.length >= this.loadFactor) {
-      resize();
+    if (this.size / this.keyMap.length >= this.loadFactor) {
+      this._resize();
     }
-    this._insert(key, value);
+    this._insert(key, value, this.keyMap);
   }
 
   get(key) {
-    const index = this._hash(key);
-    if (!this.array[index]) {
-      return `Doesn't exist`;
-    }
-    let node = this.array[index];
+    const index = this._hash(key, this.keyMap.length);
+    let node = this.keyMap[index];
     while (node) {
       if (node.key === key) {
         return node.value;
       }
       node = node.next;
     }
+    return undefined;
   }
 
   delete(key) {
-    const index = this._hash(key);
-    let node = this.array[index];
+    const index = this._hash(key, this.keyMap.length);
+    let node = this.keyMap[index];
     let prev = null;
     while (node) {
       if (node.key === key) {
         if (prev) {
           prev.next = node.next;
         } else {
-          this.array[index] = node.node;
+          this.keyMap[index] = node.next;
         }
         this.size--;
+        return node.value;
       }
       prev = node;
       node = node.next;
@@ -98,14 +100,70 @@ class HashMap {
   }
 
   has(key) {
-    const index = this._hash(key);
-    let node = this.array[index];
+    const index = this._hash(key, this.keyMap.length);
+    let node = this.keyMap[index];
     while (node) {
-      if (node.key === key) return true;
+      if (node.key === key) {
+        return true;
+      }
       node = node.next;
     }
     return false;
   }
+
+  countKeys() {
+    let count = 0;
+    for (let bucket of this.keyMap) {
+      let node = bucket;
+      while (node) {
+        count++;
+        node = node.next;
+      }
+    }
+    return count;
+  }
+
+  clear(){
+    this.keyMap = new Array(16);
+    this.size = 0;
+  }
+
+  keys() {
+    const keys = [];
+    for (let bucket of this.keyMap) {
+      let node = bucket;
+      while (node) {
+        keys.push(node.key);
+        node = node.next;
+      }
+    }
+    return keys;
+  }
+
+  values(){
+    const values = []
+    for (let bucket of this.keyMap) {
+      let node = bucket;
+      while (node) {
+        values.push(node.value)
+        node = node.next;
+      }
+    }
+    return values;
+  }
+
+  entries(){
+    const entries = [];
+    for (let bucket of this.keyMap) {
+      let node = bucket;
+      while (node) {
+        entries.push([node.key, node.value]);
+        node = node.next;
+      }
+    }
+    return entries;
+  }
 }
+
 
 export { HashMap };
--- src/index.js
+++ src/index.js
@@ -5,8 +5,16 @@ console.log(hashmap);
 hashmap.set(1, 5);
 console.log(hashmap);
 console.log(hashmap.get(1));
-hashmap.delete(1);
+console.log(hashmap.delete(1));
+hashmap.set(15645, 7);
 console.log(hashmap);
-hashmap.set(1, 5);
+
+hashmap.set('asdaihda', 2)
+hashmap.set('maria', 2)
+
+console.log(hashmap);
 
 console.log(hashmap.has(1));
+console.log(hashmap.keys());
+console.log(hashmap.values())
+console.log(hashmap.entries())
